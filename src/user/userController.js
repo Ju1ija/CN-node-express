d6f8010215baf2124a5ea3322585ee9f5490b19d
Ju1ija <j.lipnickaja@gmail.com>
2021-12-13 17:15:32 +0000

    added CRUD operations for User

---
+++ src/user/userController.js
@@ -0,0 +1,41 @@
+const User = require("./userModel");
+
+exports.addUser = async (req, res) => {
+  try {
+    const newUser = new User(req.body);
+    await newUser.save();
+    res.status(200).send({ message: "Successfully added user", newUser });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.listUsers = async (req, res) => {
+  try {
+    const list = await User.find({});
+    const message = list.length === 0 ? "No items found." : `${list.length} items found.`;
+    res.status(200).send({ message: message, list });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.updateUser = async (req, res) => {
+  try {
+    await User.findByIdAndUpdate(req.body._id, req.body);
+    const user = await User.findById(req.body._id);
+    res.status(200).send({ message: "Successfully updated user", user });
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+exports.deleteUser = async (req, res) => {
+  try {
+    const user = await User.findById(req.body._id);
+    await User.deleteOne({ _id: req.body._id });
+    res.status(200).send({ message: "Successfully deleted user", user });
+  } catch (error) {
+    console.log(error);
+  }
+}
